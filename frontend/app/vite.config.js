fc439bc73565dc123f3a3e4d37f66bf06e649830
mmarinov <mg.marinov@gmx.es>
2026-07-04 17:40:12 +0200

    refactor: update vite config file for proxy backend

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+      interval: 300,
+    },
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+      },
+      '/ws': {
+        target: 'ws://backend:3000',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
   }
 })
