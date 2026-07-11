0245b1bf99fdc26a44147279bd20e5de4c881974
Maksim Marinov <mg.marinov@gmx.es>
2026-07-11 23:22:15 +0200

    add profile visuals map

---
+++ frontend/app/src/components/profile/profileVisuals.js
@@ -0,0 +1,22 @@
+import eldwinAvatar from "@src/assets/avatars/p00.png";
+import hildaAvatar from "@src/assets/avatars/p01.png";
+import quimburAvatar from "@src/assets/avatars/p02.png";
+import gabrielAvatar from "@src/assets/avatars/p03.png";
+import eldwinPortrait from "@src/assets/characters/eldwin_portrait.jpg";
+import gabrielPortrait from "@src/assets/characters/gabriel_portrait.jpg";
+import hildaPortrait from "@src/assets/characters/hilda_portrait.jpg";
+import quimburPortrait from "@src/assets/characters/quimbur_portrait.jpg";
+
+export const CHARACTER_VISUALS = {
+  eld: { name: "Eldwin", portrait: eldwinPortrait },
+  hil: { name: "Hilda", portrait: hildaPortrait },
+  qui: { name: "Quimbur", portrait: quimburPortrait },
+  gab: { name: "Gabriel", portrait: gabrielPortrait },
+};
+
+export const CHARACTER_AVATARS = {
+  eld: eldwinAvatar,
+  hil: hildaAvatar,
+  qui: quimburAvatar,
+  gab: gabrielAvatar,
+};
